d524692cba0523427b50cc57206ab7eb24693bf1
Zahid <kronosmes@gmail.com>
2013-03-18 15:09:32 +0000

    latest

--- first_app/app/first_app.rb
+++ first_app/app/first_app.rb
@@ -1,5 +1,4 @@
-
-names = ["Zahid", "Pol Pot", "Adolf", "Elrond", "Ruben", "Leo"]
+names = ["Zahid", "Pol Pot", "Adolf", "  Ruben", "Leo", "General Pinochet"]
 
 def my_name(array)
   array.each { |name| puts name }
@@ -7,10 +6,27 @@ def my_name(array)
 end
 
 def random_name(array)
-  name = array[rand(array.length)]
-  puts name
-  name2 = array[rand(array.length)]
-  puts name2
+  #name = array[rand(array.length)]
+  #puts name
+  #name2 = array[rand(array.length)]
+  #puts name2
+
+  puts array.shuffle[0..1]
+
+end
+
+def name_letters(array)
+
+#  names = array
+#  for name in names
+#    if name.length > 6
+#      puts name
+#    else
+#      puts " "
+#    end
+#  end
+
+puts array.select { |name| name.length > 6 }
 end
 
 
@@ -19,4 +35,7 @@ end
 
 
 # my_name(names)
+
 random_name(names)
+
+#name_letters(names)
